009485e194980ec8457d954bd2767c3004b70597
utsavvaghela2904 <utsavvaghela2904@gmail.com>
2025-03-21 18:08:24 +0530

    Updates

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', (event) => {
+    console.log('Service Worker Installed');
+  });
+  
+  self.addEventListener('activate', (event) => {
+    console.log('Service Worker Activated');
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    console.log('Fetching:', event.request.url);
+  });
+  
